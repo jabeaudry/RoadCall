2e855d9b688d721db1a8953c280312bd42610a40
jabeaudry <jacintheb27@gmail.com>
2022-11-19 12:10:28 -0500

    MASSIVE UPDATE

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -8,6 +8,8 @@ interface MainProps {}
 interface MainState {
   lat: Number;
   long: Number;
+  connected: Boolean;
+  connectWithSomeone: Boolean;
 }
 
 export class Main extends React.Component<MainProps, MainState> {
@@ -16,6 +18,8 @@ export class Main extends React.Component<MainProps, MainState> {
     this.state = {
       long: 0,
       lat: 0,
+      connected: false,
+      connectWithSomeone: false,
     };
   }
 
@@ -42,15 +46,46 @@ export class Main extends React.Component<MainProps, MainState> {
   }
 
   render() {
-    return (
-      <>
-        <p>Welcome </p>
-        <Button size="large" variant="contained">
-          CONNECT
-        </Button>
-        <h1>{`${this.state.lat}`}</h1>
-        <h1>{`${this.state.long}`}</h1>
-      </>
-    );
+    if (this.state.connected) {
+      return (
+        <>
+          <h1>Welcome to RoadCall</h1>
+          {this.state.connectWithSomeone && (
+            <h2>
+              You are talking with <span>Alexandre</span>!
+            </h2>
+          )}
+          {!this.state.connectWithSomeone && <h2>Searching...</h2>}
+          <Button
+            size="large"
+            variant="contained"
+            onClick={() => {
+              this.setState({ connected: false });
+            }}
+          >
+            DISCONNECT
+          </Button>
+          <h1>{`${this.state.lat}`}</h1>
+          <h1>{`${this.state.long}`}</h1>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h1>Welcome to RoadCall</h1>
+          <h2>Connect and start talking</h2>
+          <Button
+            size="large"
+            variant="contained"
+            onClick={() => {
+              this.setState({ connected: true });
+            }}
+          >
+            CONNECT
+          </Button>
+          <h1>{"penis"}</h1>
+        </>
+      );
+    }
   }
 }
