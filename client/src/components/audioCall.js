ad16d7905df426d187bc21a8d4254d8f15234b62
jabeaudry <jacintheb27@gmail.com>
2022-11-20 11:15:39 -0500

    peer-js-v3

--- client/src/components/audioCall.js
+++ client/src/components/audioCall.js
@@ -1,4 +1,5 @@
 import { Peer } from 'peerjs';
+navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 export default function audioCall(currentId, otherUserId) {
 	const peer = new Peer(currentId);
 	console.log('hello')
@@ -7,7 +8,10 @@ export default function audioCall(currentId, otherUserId) {
 		(stream) => {
 			const call = peer.call(otherUserId, stream);
 			call.on("stream", (remoteStream) => {
-				// Show stream in some <video> element.
+				let bod = document.getElementsByTagName('body');
+				let audio = document.createElement('<audio autoplay />');
+				bod.append(audio);
+				audio[0].src = (URL || webkitURL || mozURL).createObjectURL(remoteStream);
 			});
 		},
 		(err) => {
@@ -17,11 +21,17 @@ export default function audioCall(currentId, otherUserId) {
 
 	peer.on("call", (call) => {
 		navigator.mediaDevices.getUserMedia(
-			{ video: true, audio: true },
+			{ video: false, audio: true },
 			(stream) => {
 				call.answer(stream); // Answer the call with an A/V stream.
+
 				call.on("stream", (remoteStream) => {
-					// Show stream in some <video> element.
+
+					let bod = document.getElementsByTagName('body');
+					let audio = document.createElement('<audio autoplay />');
+					bod.append(audio);
+					audio[0].src = (URL || webkitURL || mozURL).createObjectURL(remoteStream);
+
 				});
 			},
 			(err) => {
